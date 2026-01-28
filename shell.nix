70b2ab6607714068940d0108e8a06e1e8d46f187
conao3 <conao3@gmail.com>
2026-01-28 22:09:19 +0900

    specify version

--- shell.nix
+++ shell.nix
@@ -3,7 +3,12 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/13b0f9e6ac78abbbb736c635d87845c4f4bee51b.tar.gz";
     sha256 = "0js8a1abl5mi1j0l9wiasis8srm1a7wjrd3scwb8xpq3i0amvz8r";
   };
-  pkgs = import nixpkgs {};
+  overlay = final: prev: {
+    emacs = prev.emacs30;
+  };
+  pkgs = import nixpkgs {
+    overlays = [overlay];
+  };
 in
 
 pkgs.mkShell {
